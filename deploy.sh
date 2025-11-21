1e666d0c9c89c5c5e1dab77c89bab1b87a327c58
aj-223 <theshepherd.1322@gmail.com>
2025-11-21 17:50:12 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -215,7 +215,7 @@ BACKEND_SOURCE='{
 }'
 
 ARTIFACTS='{"type":"NO_ARTIFACTS"}'
-SOURCE_VERSION="updatedUI"
+SOURCE_VERSION="Mohamed"
 
 echo "Creating Backend CodeBuild project '$BACKEND_PROJECT_NAME'..."
 aws codebuild create-project \
