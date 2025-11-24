300bb13f458f209c5de155bb8b871b3bdc4678e0
aj-223 <theshepherd.1322@gmail.com>
2025-11-23 20:25:51 -0500

    Remediation 1.2

--- cdk_backend/lib/cdk_backend-stack.ts
+++ cdk_backend/lib/cdk_backend-stack.ts
@@ -133,7 +133,7 @@ export class CdkBackendStack extends cdk.Stack {
         requireDigits: true,
         requireSymbols: true, // CHANGE
         requireUppercase: true, // CHANGE
-        tempPasswordValidity: 7, // CHANGE
+         tempPasswordValidity: cdk.Duration.days(7) //Updated
       },
       standardAttributes: {
         email: { required: true, mutable: true },
